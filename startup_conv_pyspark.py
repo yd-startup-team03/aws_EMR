62ac3bb9fbeab56e5a6eeb840d50a0aaba267480
je0nh <winter0pear@gmail.com>
2023-11-27 17:32:12 +0900

    schema 분리

--- startup_conv_pyspark.py
+++ startup_conv_pyspark.py
@@ -1,4 +1,3 @@
-import re
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import expr, col, column
 
@@ -16,30 +15,62 @@ df.show(5)
 df.select(col("properties.name")).show()
 
 # column 불러오기
-dfType = df.dtypes[1:]
+segment_datatypes = df.dtypes[1:]
 
-# 정규식 표현
-"""
-중간에 nested column이 있어서 name:struct<con:string,term:struct<for:string>.ip:string> 식으로 표현되는거 [('name', struct), ('con' : 'string'), ('term', 'struct'), ('for', 'string'), ('ip', 'string')] << 식으로 바꿔줌
-"""
-def parse_struct_string(struct_str):
-    pattern = re.compile(r'([a-zA-Z0-9_]+):(\b\w+\b)')
-    matches = pattern.findall(struct_str)
+segment_datatypes = [('datatype', 'string'),
+                     ('text', 'struct<datatype:struct<datatype:string,datatype:string>,datatype:string,datatype:struct<datatype:string>,datatype:string>'),
+                     ('ent', 'string'), ('mesd', 'string'),
+                     ('pop', 'struct<cat:string,pat:string,pop:struct<datatype:struct<sot:string>,rel:string>,ref:string>'),
+                     ('tray', 'struct<Id:string>')]
 
-    return matches
+import re
 
-colName = []
+schema = []
 
-for i in range(len(dfType)):
-    if 'struct' in dfType[i][1]:
-        for j in parse_struct_string(dfType[i][1]):
-            if 'struct' in j[1]:
-                for column in df.select(f"{dfType[i][0]}.{j[0]}.*").columns:
-                    colName.append(f"{dfType[i][0]}.{column}.{j[0]}")
+def properties_datatype(nested_col):
+    col = []
+    _full_field = nested_col[:nested_col.find("<")] + nested_col[nested_col.rfind(">")+1:]
+    full_field = _full_field.split(',')
+    for _col in full_field:
+        col.append(_col.split(':')[0])
+    nes_col = nested_col[nested_col.find("<")+1:nested_col.rfind(">")]
+    return col, full_field, nes_col
+
+for schema_name, schema_datatype in segment_datatypes:
+    if schema_name == 'text':
+        schema_datatype = schema_datatype[schema_datatype.find('<')+1:]
+        matches = re.findall(r'[^,<>]+<[^<>]+>|[^,<>]+', schema_datatype)
+        for matche in matches:
+            if 'struct' in matche:
+                up_schema = matche[:matche.find(':')]
+                down_schema_ls = matche[matche.find('<')+1:matche.find('>')]
+                for _down_schema in down_schema_ls.split(','):
+                    down_schema = _down_schema[:_down_schema.find(':')]
+                    schema.append(f'{schema_name}.{up_schema}.{down_schema}')
             else:
-                for column in df.select(f"{dfType[i][0]}.*").columns:
-                    colName.append(f"{dfType[i][0]}.{column}")
+                up_schema = matche[:matche.find(':')]
+                schema.append(f'{schema_name}.{up_schema}')
+    
+    elif schema_name == 'pop':
+        if 'struct' in schema_datatype:
+            schema_datatype = schema_datatype[schema_datatype.find("<")+1:schema_datatype.rfind(">")]
+            col, full_field, nes_col = properties_datatype(schema_datatype)
+            for i in range(len(col)):
+                if 'struct' in full_field[i]:
+                    col_1, full_field_1, nes_col_1 = properties_datatype(nes_col)
+                    for j in range(len(col_1)):
+                        if 'struct' in full_field_1[j]:
+                            nes_col_1_ls = nes_col_1.split(',')
+                            for k in nes_col_1_ls:
+                                nes_col_1_ls = k[:k.find(':')]
+                                schema.append(f'{schema_name}.{col[i]}.{col_1[j]}.{nes_col_1_ls}')
+                        else:
+                            schema.append(f'{schema_name}.{col[i]}.{col_1[j]}')
+                else:
+                    schema.append(f'{schema_name}.{col[i]}')
+    
+    elif schema_name == 'tray':
+        schema.append(f'{schema_name}.Id')
+    
     else:
-        colName.append(dfType[i][0])
-
-colName
+        schema.append(schema_name)
